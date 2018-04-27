fb2d0540c01717654a3c5f1f27037de83e58456a
liphenste <liphenste@gmail.com>
2018-04-27 19:18:53 -0400

    finish js component; still need to fix bpm algorithm

--- app/app.js
+++ app/app.js
@@ -2,17 +2,27 @@ var bpmApp = angular.module('bpmApp', ['ngRoute']);
 
 bpmApp.controller('bpmCtrl', function ($scope) {
   $scope.times = [];
-  $scope.MAX_LEN = 5;
+  $scope.MAX_LEN = 4;
+  $scope.SpMS = 1 / 1000; //seconds per ms
+  $scope.MpS = 1 / 60; //min per sec
   $scope.bpm = 0;
 
   $scope.set_bpm = function() {
     $scope.times.push(performance.now()); //measures time
     var len = $scope.times.length;
-    if (len >= 2) {
-      $scope.bpm = ($scope.times[len-1] - $scope.times[0]) / len;
+    if (len == 1) {
+      $scope.bpm = 0;
+      document.getElementById("firstClick").innerHTML = "(first click!)";
+    } else {
+      document.getElementById("firstClick").innerHTML = "";
+    $scope.bpm = Math.round(len / 
+      (($scope.times[len-1] - $scope.times[0]) * $scope.SpMS * $scope.MpS));
     }
-    if (len >= $scope.MAX_LEN) {
-      times.shift(); //removes first time from times (dynamic array)
-    }
+    if (len > $scope.MAX_LEN) $scope.times.shift(); //removes first time from times (dynamic array)
   };
+
+  $scope.reset_bpm = function() {
+    $scope.times = [];
+    $scope.bpm = 0;
+  }
 });
