42b69d24ac7e077dc3db703d2b33ea681c2e4ffb
liphenste <liphenste@gmail.com>
2018-04-28 14:22:33 -0400

    add reset delay

--- app/app.js
+++ app/app.js
@@ -7,6 +7,9 @@ bpmApp.controller('bpmCtrl', function ($scope) {
   $scope.MpS = 1 / 60; //min per sec
   $scope.bpm = 0;
 
+  $scope.RESET_TIME = 3000; //ms
+  $scope.reset_timer = window.setTimeout($scope.reset_bpm, $scope.RESET_TIME);
+
   $scope.set_bpm = function() {
     $scope.times.push(performance.now()); //measures time
     var len = $scope.times.length;
@@ -19,16 +22,26 @@ bpmApp.controller('bpmCtrl', function ($scope) {
       (($scope.times[len-1] - $scope.times[0]) * $scope.SpMS * $scope.MpS));
     }
     if (len > $scope.MAX_LEN) $scope.times.shift(); //removes first time from times (dynamic array)
-    console.log($scope.times.toString());
+
+    $scope.reset();
+    //console.log($scope.times.toString());
   };
 
   $scope.reset_bpm = function() {
     $scope.times = [];
     $scope.bpm = 0;
+    $scope.$apply(); //forces a digest cycle; need to figure out how to use $timeout..!
+    //console.log($scope.times.toString() + " ;");
   }
 
-  $scope.blurMethod = function(buttonNumber) {
+  $scope.reset = function() {
+    clearTimeout($scope.reset_timer);
+    $scope.reset_timer = window.setTimeout($scope.reset_bpm, $scope.RESET_TIME);
+    //console.log("resetted timer");
+  }
+
+  $scope.blur_method = function(buttonNumber) {
     document.getElementsByClassName("btn")[buttonNumber].blur();
-    console.log("blurred");
+    //console.log("called blur_method");
   }
 });
